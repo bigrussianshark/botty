c5135c0c3457d947e32e57bfe7c1b6d1bbed7628
bigrussianshark <33806777+bigrussianshark@users.noreply.github.com>
2017-11-19 17:38:15 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Token of your telegram bot
+# (https://core.telegram.org/bots#6-botfather)
+TELEGRAM_TOKEN = '503640985:AAHb8su9kQ92vYmesCEVSa4fKaL2q2HpR6I'
+
+# Credtals for logging to vk
+VK_LOGIN = '+79992439581'
+VK_PASS = 'bitchebitch'
+
+# List of admins and moders
+# Example: [ '66303244', 'vk1' ]
+ADMINS_IDS = [ 'cryogenn' ]
+MODERS_IDS = ADMINS_IDS + [  ]
+
+DATABASE_PATH = 'db.json'
+USERS_PATH = 'users'
